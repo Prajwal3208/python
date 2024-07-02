afe43a02b9373a30bd5a7e590b262a34bdc7e9f7
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-02 15:53:42 +0530

    Create day15.py

---
+++ python_course/day15.py
@@ -0,0 +1,20 @@
+import time
+
+t = time.strftime('%H:%M:%S')
+print(t)
+hours = int(time.strftime('%H'))
+
+minits = int(time.strftime('%M'))
+
+sec = int(time.strftime('%S'))
+
+
+if(hours >=0 and hours<12):
+  print('Good Morning')
+elif(hours >=12 and hours <18):
+  print ('good afternoon')
+elif(hours <=18 and hours<21):
+  print ('good evening')
+else:
+  print('good night')
+  
