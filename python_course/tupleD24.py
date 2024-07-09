865aa64b357e64a6f4a35c6a0bbdf4464cedf51f
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-09 17:36:55 +0530

    Create tupleD24.py

---
+++ python_course/tupleD24.py
@@ -0,0 +1,33 @@
+tuple = (1,2,"prajwal","kadam",0.3333)
+
+# print(tuple)
+
+# print(len(tuple))
+# print(tuple[0])
+
+# print(tuple[-3])
+# print(tuple[len(tuple)-3])
+# if "prajwal" in tuple:
+#   print("yes")
+# else:
+#   print("no")
+print(tuple[0:5:2])
+
+
+  
+# tup = (1, 2, 76, 342, 32, "green", True)
+# # tup[0] = 90
+# print(type(tup), tup)
+# print(len(tup))
+# print(tup[0])
+# print(tup[2])
+# # print(tup[34])
+
+# if  3421 in tup:
+#   print("Yes 342 is present in this tuple")
+# tup2 = tup[1:4]
+# print(tup2)
+
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+print(animals[4:])      #using positive indexes
+print(animals[-4:])     #using negative indexes
