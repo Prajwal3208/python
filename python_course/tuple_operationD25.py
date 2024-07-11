073b339f285bc1907b012bc72c3cbbdcfd441787
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-11 22:47:03 +0530

    Create tuple_operationD25.py

---
+++ python_course/tuple_operationD25.py
@@ -0,0 +1,19 @@
+
+# countries = ("Spain", "Italy", "India", "England", "Germany")
+# temp = list(countries)
+# temp.append("Russia")       #add item 
+# temp.pop(3)                 #remove item
+# temp[2] = "Finland"         #change item
+# countries = tuple(temp)
+# print(countries)
+# countries1 = ("Pakistan", "Afghanistan", "Bangladesh", "ShriLanka")
+# countries2 = ("Vietnam", "India", "China")
+# southEastAsia = countries1 + countries2
+# print(southEastAsia)
+
+tuple =  (1,2,2,3,10,20,4,7,90,222)
+# res = tuple.count(3)
+# res = tuple.index(222)
+# res = tuple.index(222,5)
+# res = tuple.index(4,5,9)
+print(res)
