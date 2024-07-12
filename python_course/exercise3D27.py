7ec2e24a5958933f7612f22d9ea49f541c06fe19
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-12 23:15:06 +0530

    Create exercise3D27.py

---
+++ python_course/exercise3D27.py
@@ -0,0 +1,21 @@
+print("what is capital of india")
+option = ["delhi","mumbai","kolkata","chennai"]
+print(option)
+
+answer = input("enter your answer:")
+
+if answer == "delhi":
+    print("correct answer you won 1000rs")
+else:
+    print("Wrong answer")
+
+print("what is most popular language in india")
+option = ["marathi","gujrati","hindi","english"]
+print(option)
+
+answer = input("enter your answer:")
+
+if answer == "hindi":
+    print("correct answer you won 10000rs")
+else:
+    print("Wrong answer")
