58a69208b37b63efdee0873e5b4dd81b127508b7
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-20 00:43:50 +0530

    Create SertD31.py

---
+++ python_course/SertD31.py
@@ -0,0 +1,18 @@
+s = {2, 4, 2, 6}
+
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+for value in info:
+  print(value)
+
+###***Quick Quiz: Try to create an empty set. Check using the type() function whether the type of your variable is a set
+
+
+# harry = {}
+# print(type(harry))
+
+harry = set()
+print(type(harry))
