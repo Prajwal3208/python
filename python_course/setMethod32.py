da2cefaad11be42f4268967cc88aca4985117412
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-21 16:34:03 +0530

    Create setMethod32.py

---
+++ python_course/setMethod32.py
@@ -0,0 +1,35 @@
+c = {"m","k","d","a","b","c"}
+# c2 = {"a","b","c","d"}
+# c2 = {"a","b","c","d","e","f"}
+# c.update(c2)
+# print(c)
+# c3 = c.union(c2)
+# print(c3)
+# c3 = c.intersection(c2)
+# print(c3)
+# c.intersection_update(c2)
+# print(c)
+# c3 = c.symmetric_difference(c2)
+# print(c3)
+# c.symmetric_difference_update(c2)
+# print(c)
+# c3 = c.difference(c2)
+# print(c3)
+# c.difference_update(c2)
+# print(c)
+# print(c.isdisjoint(c2))
+# print(c.issuperset(c2))
+# print(c2.issubset(c))
+# item = c.pop()
+# print(c)
+# print(item)
+# del c
+# print(c)
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities.clear()
+# print(cities)
+info = {"Carla", 19, False, 5.9}
+if "Carla" in info:
+    print("Carla is present.")
+else:
+    print("Carla is absent.")
