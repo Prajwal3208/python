79544a5318863fb0aa05bc115d1925c79d2eb22d
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-21 18:46:45 +0530

    Create Dictionary_Day33.py

---
+++ python_course/Dictionary_Day33.py
@@ -0,0 +1,18 @@
+info= {'name':'Karan', 'age':19, 'eligible':True}
+# print(inf) 
+# print(inf.keys())
+# print(inf.values())
+# print(inf["name"])
+# print(inf.get("age"))
+# print(inf.values())
+# print(inf.keys()) 
+# print(inf["name"])
+# print(inf.items())
+
+
+# print(info.items())
+# for key, value in info.items():
+#   print(f"The value corresponding to the key {key} is {value}") 
+
+for key in info.keys():
+  print(f"The value of corresponding key is {key} value is {info[key]}")
