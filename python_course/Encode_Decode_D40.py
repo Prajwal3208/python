ff9219d39438c40217bee4cb7d29d671bdfbf599
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-25 17:51:43 +0530

    Create Encode_Decode_D40.py

---
+++ python_course/Encode_Decode_D40.py
@@ -0,0 +1,30 @@
+
+
+def encode(message):
+  if len(message) <= 2:
+    message_revers = message[::-1]
+    print(message_revers)
+  else:
+    new_msg = message[1:] + message[0]
+    print("THE DECODED MSG IS :", "xyz" + new_msg + "abc")
+    
+
+def decode(message):
+    if len(message) <= 2:
+      message_revers = message[::-1]
+      print(message_revers)
+    else:
+      new_msg = message[3:-3]
+      print("THE ORIGINAL MSG IS :",(new_msg[-1] + new_msg[:-1]))
+
+option = int(input("Enter 1 for encode and 2 for decode: "))
+
+print(option)
+message = input("Enter the msg str only : ")
+if option == 1:
+  print(encode(message))
+elif option == 2:
+  print(decode(message))
+else:
+  print("invalid input ")
+  
