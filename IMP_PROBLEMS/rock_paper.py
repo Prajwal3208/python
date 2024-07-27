a21653857c12ffc63592e0d8610eff17e89e7344
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-27 18:55:43 +0530

    Create rock_paper.py

---
+++ IMP_PROBLEMS/rock_paper.py
@@ -0,0 +1,10 @@
+a = input("Enter the input of 'A':")
+if (a.lower()== "paper"):
+    print("B: sizer")
+elif (a.lower()== "sizer"):
+    print("B: rock")
+elif (a.lower()== "rock"):
+    print("B: paper")
+else:
+    print("Invalid Input")
+    
