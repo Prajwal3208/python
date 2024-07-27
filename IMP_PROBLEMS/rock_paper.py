f10ad166e9e5d70bdd07905ce5027ee0fb02c178
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-07-27 19:09:52 +0530

    Update rock_paper.py

--- IMP_PROBLEMS/rock_paper.py
+++ IMP_PROBLEMS/rock_paper.py
@@ -1,10 +1,30 @@
-a = input("Enter the input of 'A':")
-if (a.lower()== "paper"):
-    print("B: sizer")
-elif (a.lower()== "sizer"):
-    print("B: rock")
-elif (a.lower()== "rock"):
-    print("B: paper")
-else:
-    print("Invalid Input")
+# a = input("Enter the input of 'A':")
+# if (a.lower()== "paper"):
+#     print("B: sizer")
+# elif (a.lower()== "sizer"):
+#     print("B: rock")
+# elif (a.lower()== "rock"):
+#     print("B: paper")
+# else:
+#     print("Invalid Input")
     
+
+def winb(a):
+    
+    if (a.lower()== "paper"):
+        print("B: sizer")
+    elif (a.lower()== "sizer"):
+        print("B: rock")
+    elif (a.lower()== "rock"):
+        print("B: paper")
+    else:
+        print("Invalid Input")
+    
+while True:
+    a = input("Enter the input of 'A'('rock','paper','sizer') or 'quit' for exit:")
+    if a.lower()== "quit":
+        print("you are exited")
+        break
+    else :
+        winb(a)
+        
