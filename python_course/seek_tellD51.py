59abfdde4a36351cf1be9b6a1ea5d3015ee1ad22
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-08-03 22:51:17 +0530

    Create seek_tellD51.py

---
+++ python_course/seek_tellD51.py
@@ -0,0 +1,30 @@
+# with open('sample.txt', 'w') as f:
+#   f.write('Hello World3!')
+#   f.truncate(3)
+
+# with open('sample.txt', 'r') as f:
+#   print(f.read())
+
+# with open('pra.txt','w') as f:
+#   f.write("hye my name is prajwal i am from deur tel koregoan dist satara dlalalallalalalllllllllllllllllllll")
+  
+# # with open('pra.txt','r') as f:
+# #   f.seek(4)
+# #   print(f.read(10))
+
+# with open("pra.txt","r") as f:
+#   data = f.read(10)
+#   current_po = f.tell()
+#   f.seek(current_po)
+#   print(current_po)
+
+#   print(data)
+#   p = f.read(5)
+#   print(p)
+with open('sample.txt', 'w') as f:
+  f.write('Hello World!')
+  f.truncate(8)
+
+with open('sample.txt', 'r') as f:
+  print(f.read())
+  
